f14d5babb78918ebace2c9109f6540dc95c89fca
Alissa Wilson <Alissa@Alissas-MacBook-Air.local>
2016-11-02 22:50:43 -0400

    started part B

--- HW3-alissalw/bshw3.py
+++ HW3-alissalw/bshw3.py
@@ -7,6 +7,48 @@
 # 2) Replace the main picture with a picture of yourself.
 # 3) Replace any local images with the image I provided in media.  (You
 # must keep the image in a separate folder than your html code.
+from bs4 import BeautifulSoup
+import requests
+import re
+
+base_url = "https://www.si.umich.edu/programs/bachelor-science-information/bsi-admissions"
+r = requests.get(base_url)
+soup = BeautifulSoup(r.text)
+for i in soup.find_all(text=True):
+	if "student" in i:
+		i = i.split(" ")
+		for word in i:
+			if word == 'students':
+				word = "AMAZING students"
+for i in soup.find_all("iframe"):
+	i['src'] = "https://scontent-ort2-1.xx.fbcdn.net/l/t31.0-8/12961377_1070535396341586_8029777897783604764_o.jpg"
+
+print(soup)
+
+
+
+
+
+# for line in text:
+# 	line = line.rstrip()
+# 	if "student" in line:
+# 		for word in line.split(" "):
+# 			if word == "student":
+# 				word == "AMAZING student"
+# 			if word == "students":
+# 				word = "AMAZING students"
+# 		print(line)
+
+
+
+
+# soup(text=re.compile("student"))
+# for instance in s:
+# 	for word in instance:
+# 		if word == "student":
+# 			word.replace("student", "AMAZING student")
+
+
 
 # Deliverables
 # Make sure the new page is uploaded to your GitHub account.
